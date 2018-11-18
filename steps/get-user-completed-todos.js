960924285641cf7195ad945de427518782ec9c7a
Uzo Olisemeka <uzo@designbymobius.ca>
2018-11-18 08:06:30 -0500

    new step + task: get viewer completed todos

---
+++ steps/get-user-completed-todos.js
@@ -0,0 +1,24 @@
+var send_api_request = require( './send-api-request' );
+
+module.exports = function( task, config ){
+  if ( ! config ) config = {};
+
+  var user_id = config.user_id;
+  send_api_request( task, { request: { query: get_user_completed_todos_query( user_id ) }});
+}
+
+function get_user_completed_todos_query( user_id ){
+  if( ! user_id ) throw new Error( 'user id not specified' );
+
+  return ''
+    + '{'
+      + 'user(id: ' + user_id + ') {'
+        + 'todos(completed: true, limit: 999999) {'
+          + 'id '
+          + 'body '
+          + 'created_at '
+          + 'completed_at '
+        + '}'
+      + '}'
+    + '}';
+}
