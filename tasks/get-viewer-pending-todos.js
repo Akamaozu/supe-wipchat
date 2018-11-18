572e3cd05975a5b14d42abac741ee4d17ee2ba8d
Uzo Olisemeka <uzo@designbymobius.ca>
2018-11-18 07:56:40 -0500

    new task: get viewer's pending todos

---
+++ tasks/get-viewer-pending-todos.js
@@ -0,0 +1,19 @@
+module.exports = function( task, config ){
+
+  require('./steps/get-viewer')( task );
+
+  task.step( 'extract viewer user id', function(){
+    var res = task.get( 'wipchat-api-response' ),
+        user_id = res.data.viewer.id;
+
+    task.set( 'wipchat-api-viewer-user-id', user_id );
+    task.next();
+  });
+
+  task.step( 'get viewer pending todos', function(){
+    var viewer_user_id = task.get( 'wipchat-api-viewer-user-id' );
+
+    require( './steps/get-user-pending-todos' )( task, { user_id: viewer_user_id });
+    task.next();
+  });
+}
